f719ffe8159c6354c0eab4e518bfe2ba44e760b5
KaremMetrial <karim.metrial@gmail.com>
2025-05-26 22:15:49 +0300

    modifie frontend login page

--- resources/views/admin/layout/scripts.blade.php
+++ resources/views/admin/layout/scripts.blade.php
@@ -15,3 +15,35 @@
 <!-- Template JS File -->
 <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
 <script src="{{ asset('admin/assets/js/custom.js') }}"></script>
+
+<!--toastr js-->
+<script src="{{ asset('admin/js/toastr.min.js') }}"></script>
+
+<script>
+    // Success Message
+    @if(Session::has('success'))
+    toastr.success("{{ Session::get('success') }}")
+    @endif
+
+    // Info Message
+    @if(Session::has('info'))
+    toastr.info("{{ Session::get('info') }}")
+    @endif
+
+    // Warning Message
+    @if(Session::has('warning'))
+    toastr.warning("{{ Session::get('warning') }}")
+    @endif
+
+    // Error Message
+    @if(Session::has('error'))
+    toastr.error("{{ Session::get('error') }}")
+    @endif
+
+    // Validation Errors
+    @if($errors->any())
+    @foreach($errors->all() as $error)
+    toastr.error("{{ $error }}")
+    @endforeach
+    @endif
+</script>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,4 @@
+{{--
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -26,7 +27,7 @@
 
         <!-- Remember Me -->
         <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
+                <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
@@ -45,3 +46,101 @@
         </div>
     </form>
 </x-guest-layout>
+--}}
+@extends('frontend.layout.master')
+@section('content')
+    <!--=============================
+    BREADCRUMB START
+==============================-->
+    <section class="fp__breadcrumb" style="background: url(frontend/images/counter_bg.jpg);">
+        <div class="fp__breadcrumb_overlay">
+            <div class="container">
+                <div class="fp__breadcrumb_text">
+                    <h1>sign in</h1>
+                    <ul>
+                        <li><a href="{{ route('home') }}">home</a></li>
+                        <li><a href="#">sign in</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=============================
+        BREADCRUMB END
+    ==============================-->
+
+
+    <!--=========================
+        SIGNIN START
+    ==========================-->
+    <section class="fp__signin" style="background: url(frontend/images/login_bg.jpg);">
+        <div class="fp__signin_overlay pt_125 xs_pt_95 pb_100 xs_pb_70">
+            <div class="container">
+                <div class="row wow fadeInUp" data-wow-duration="1s">
+                    <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
+                        <div class="fp__login_area">
+                            <h2>Welcome back!</h2>
+                            <p>sign in to continue</p>
+                            <form method="POST" action="{{ route('login') }}" class="fp__login_form">
+                                @csrf
+                                <div class="row">
+
+                                    <!-- Email section -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label for="email">email</label>
+                                            <input type="email" placeholder="Email" name="email"
+                                                   value="{{ old('email') }}" required autofocus>
+                                        </div>
+                                    </div>
+                                    <!-- ./Email section -->
+
+                                    <!-- Password section -->
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <label for="password">password</label>
+                                            <input type="password" placeholder="Password" name="password" required
+                                                   autocomplete="current-password">
+                                        </div>
+                                    </div>
+                                    <!-- ./Password section -->
+
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput fp__login_check_area">
+                                            <!-- Remember Me -->
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" name="remember"
+                                                       id="remember_me">
+                                                <label class="form-check-label" for="remember_me">
+                                                    Remeber Me
+                                                </label>
+                                            </div>
+                                            <!-- ./Remember Me -->
+
+                                            @if (Route::has('password.request'))
+                                                <!-- Forget Password -->
+                                                <a href="{{ route('password.request') }}">Forgot Password ?</a>
+                                                <!-- ./Forget Password -->
+                                            @endif
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="fp__login_imput">
+                                            <button type="submit" class="common_btn">login</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <p class="create_account">Dont’t have an aceount ? <a href="sign_up.html">Create Account</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=========================
+        SIGNIN END
+    ==========================-->
+
+@endsection
